7b4e67bd61ac7c1d1ed599f0e79b40919b4ba07d
Rohit Bhoompally <rohit.bhoompally@gmail.com>
2014-11-25 04:41:57 -0500

    Splash Screen UI in-place.

---
+++ app/src/main/java/me/widea/taketurns/SplashActivity.java
@@ -0,0 +1,70 @@
+package me.widea.taketurns;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+
+import me.widea.taketurns.beans.LoginActivity;
+
+/**
+ * Created by Rohit and Viveka on 11/23/14.
+ */
+
+public class SplashActivity extends Activity {
+    ImageView ivTurns;
+    RotateAnimation rotateAnimation;
+    ScaleAnimation scaleAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ivTurns = (ImageView) findViewById(R.id.as_iv_turns);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                /**
+                 * Check for the oAuth token. If it is still valid, fetch user details and take him to the main activity
+                 */
+//                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+//                startActivity(i);
+//                finish();
+
+                /** Else show login screen **/
+                Intent loginIntent = new Intent(SplashActivity.this, LoginActivity.class);
+            }
+        }, 3000);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        rotateAnimation = new RotateAnimation(0f, 120f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimation.setInterpolator(new LinearInterpolator());
+        rotateAnimation.setFillAfter(true);
+        rotateAnimation.setFillEnabled(true);
+        rotateAnimation.setRepeatCount(Animation.INFINITE);
+        rotateAnimation.setStartOffset(700);
+        rotateAnimation.setDuration(500);
+        ivTurns.startAnimation(rotateAnimation);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        ivTurns.clearAnimation();
+    }
+}
---
+++ app/src/main/java/me/widea/taketurns/beans/LoginActivity.java
@@ -0,0 +1,30 @@
+package me.widea.taketurns.beans;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Rohit and Viveka on 11/25/14.
+ */
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
